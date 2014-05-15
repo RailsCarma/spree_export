5eaad0d001635dac27d7984f0eb7b9291d9227fb
RailsCarma <github@carmatec.com>
2014-05-15 11:41:08 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Spree::Core::Engine.routes.draw do
   get '/admin/users_export_csv/', to: 'admin/users#export_csv', as: "export_users_csv"
-  get '/admin/orders/export_csv/', to: 'admin/orders#export_csv', as: "export_csv"
+  get '/admin/orders_export.csv/', to: 'admin/orders#export_csv', as: "export_orders_csv"
+
 end
